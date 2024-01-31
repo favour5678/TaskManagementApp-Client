ec523ff7bbd964d4a47c6c88e54eb5fddf3dbab8
Oshoaro Favour <oshoarofavour@gmail.com>
2024-01-31 16:26:24 +0100

    changed the font

--- app/page.js
+++
@@ -1,5 +0,0 @@
-export default function Home() {
-  return (
-   <div>Hello Favour, welcome to next.js</div>
-  );
-}
---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+   <main>
+  <div>God is good, all the time!</div>
+   </main>
+  );
+}
