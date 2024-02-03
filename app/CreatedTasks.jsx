614ce1577ef7637ecacbdc044e4fd28381ea21f0
Oshoaro Favour <oshoarofavour@gmail.com>
2024-02-03 21:27:50 +0100

    downloaded a pckage and working on the createdtask component

--- app/CreatedTasks.jsx
+++ app/CreatedTasks.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { FaRegEdit } from "react-icons/fa";
+import { BsTrash3Fill } from "react-icons/bs";
 
 export default function CreatedTasks() {
   return (
     <div>
         <div></div>
-        
+        <FaRegEdit />
+        <BsTrash3Fill />
     </div>
   )
 }
