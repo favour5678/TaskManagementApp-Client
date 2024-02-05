1e2a3c6109eaef32dc3f0a2cc9e33274853614b0
Oshoaro Favour <oshoarofavour@gmail.com>
2024-02-05 11:50:30 +0100

    added usestae in homepage component & mapped my tasks in the createdtasks component

--- app/CreatedTasks.jsx
+++ app/CreatedTasks.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from "react";
 import { FaRegEdit } from "react-icons/fa";
 import { BsTrash3Fill } from "react-icons/bs";
 
-export default function CreatedTasks() {
+export default function CreatedTasks({ tasks }) {
   return (
     <div>
-        <div></div>
-        <FaRegEdit />
-        <BsTrash3Fill />
+      {tasks.map((task, index) => {
+        <div key={index}>
+          <p>{task}</p>
+          <FaRegEdit />
+          <BsTrash3Fill />
+        </div>
+      })}
     </div>
-  )
+  );
 }
--- app/HomePage.jsx
+++ app/HomePage.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
+'use client'
+
+import { useState } from 'react';
 import CreatedTasks from './CreatedTasks'
 
 export default function HomePage() {
+  const [tasks, setTasks] = useState([]);
+  
   return (
     <section>
         <h1 className="text-center text-xl pt-5">Task Management Application(T.M.A)</h1>
         <div className='ml-10 mt-10 space-y-2'>
             <input type="text" className='border outline-none px-3 border-green-900 h-10 w-[30%]' placeholder='Input task here'/>
-            <div className='font-semibold text-lg '>
+            <div className='font-semibold text-lg'>
                 <button className='bg-green-300 p-2 rounded-md'>Create Task</button>
             </div>
         </div>
         <div className='border border-green-900 mt-10 mx-10'></div>
-        <CreatedTasks />
+        
+        <CreatedTasks tasks={tasks}/>
     </section>
   )
 }
